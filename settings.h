8b3f11fb41191be8005d6e6a545be7f9a12458be
wowa <waldemar.gruenwald@ubirch.com>
2019-07-31 08:05:35 +0200

    included cJSON, cumulocity and snmp

--- settings.h
+++ settings.h
@@ -5,7 +5,7 @@
 #ifndef EXAMPLE_ESP32_SETTINGS_H
 #define EXAMPLE_ESP32_SETTINGS_H
 
-const char* AUTHORIZATION = {"Basic THISISSOMEAUTHORIZATIONYOUNEEDTOENTER!"}; // 54 char
+const char *AUTHORIZATION = {"Basic bWFuYWdlbWVudC9kZXZpY2Vib290c3RyYXA6RmhkdDFiYjFm"}; // 54 char
 const char* TENANT = {"ubirch"}; // 6 char
 const char* HOST = {"management.cumulocity.com"};  //25 char
 
